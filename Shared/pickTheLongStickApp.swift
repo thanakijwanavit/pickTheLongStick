8c64133ba76aca9f372242890c1cfc7375753a1f
Nic Wanavit <nwanavit@gmail.com>
2022-03-23 01:50:03 -0700

    Initial Commit

---
+++ Shared/pickTheLongStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickTheLongStickApp.swift
+//  Shared
+//
+//  Created by nic wanavit on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct pickTheLongStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
